927175ba6adc3a329010cb5ddd2cdfe1621471d8
obynonwane <ojohnson@polygon.technology>
2025-08-10 16:13:47 +0100

    update to retrieving history

--- cmd/api/payments.go
+++ cmd/api/payments.go
@@ -103,6 +103,230 @@ func (app *Config) PaystackTransactionInitialization(w http.ResponseWriter, r *h
 	app.writeJSON(w, http.StatusOK, payload)
 }
 
+type CancelSubscriptionPayload struct {
+	UserId string `json:"user_id"`
+}
+
+func (app *Config) CancelSubscription(w http.ResponseWriter, r *http.Request) {
+
+	// verify the user token
+	user, err := app.getToken(r)
+	if err != nil {
+		app.errorJSON(w, err, user.Data, http.StatusUnauthorized)
+		return
+	}
+
+	if user.Error {
+		app.errorJSON(w, errors.New(user.Message), user.Data, user.StatusCode)
+		return
+	}
+
+	//extract the request body
+	var requestPayload CancelSubscriptionPayload
+
+	userId := user.Data.(map[string]interface{})["user"].(map[string]interface{})["id"].(string)
+	requestPayload.UserId = userId
+
+	//create some json we will send to authservice
+	jsonData, _ := json.MarshalIndent(requestPayload, "", "\t")
+
+	invServiceUrl := fmt.Sprintf("%s%s", os.Getenv("PAYMENT_SERVICE_URL"), "cancel-subscription")
+
+	// call the service by creating a request
+	request, err := http.NewRequest("POST", invServiceUrl, bytes.NewBuffer(jsonData))
+
+	if err != nil {
+		log.Println(err)
+		app.errorJSON(w, err, nil)
+		return
+	}
+
+	// Set the Content-Type header
+	request.Header.Set("Content-Type", "application/json")
+	//create a http client
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		log.Println(err)
+		app.errorJSON(w, err, nil)
+		return
+	}
+	defer response.Body.Close()
+
+	// create a varabiel we'll read response.Body into
+	var jsonFromService jsonResponse
+
+	// decode the json from the auth service
+	err = json.NewDecoder(response.Body).Decode(&jsonFromService)
+	if err != nil {
+		app.errorJSON(w, err, nil)
+		return
+	}
+
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New(jsonFromService.Message), nil, response.StatusCode)
+		return
+	}
+
+	var payload jsonResponse
+	payload.Error = jsonFromService.Error
+	payload.StatusCode = jsonFromService.StatusCode
+	payload.Message = jsonFromService.Message
+	payload.Data = jsonFromService.Data
+
+	app.writeJSON(w, http.StatusOK, payload)
+}
+
+type SubscriptionHistoryPayload struct {
+	UserId string `json:"user_id"`
+}
+
+func (app *Config) GetSubscriptionHistory(w http.ResponseWriter, r *http.Request) {
+
+	// verify the user token
+	user, err := app.getToken(r)
+	if err != nil {
+		app.errorJSON(w, err, user.Data, http.StatusUnauthorized)
+		return
+	}
+
+	if user.Error {
+		app.errorJSON(w, errors.New(user.Message), user.Data, user.StatusCode)
+		return
+	}
+
+	//extract the request body
+	var requestPayload SubscriptionHistoryPayload
+
+	userId := user.Data.(map[string]interface{})["user"].(map[string]interface{})["id"].(string)
+	requestPayload.UserId = userId
+
+	//create some json we will send to authservice
+	jsonData, _ := json.MarshalIndent(requestPayload, "", "\t")
+
+	invServiceUrl := fmt.Sprintf("%s%s", os.Getenv("PAYMENT_SERVICE_URL"), "subscription-history")
+
+	// call the service by creating a request
+	request, err := http.NewRequest("POST", invServiceUrl, bytes.NewBuffer(jsonData))
+
+	if err != nil {
+		app.errorJSON(w, err, nil)
+		return
+	}
+
+	// Set the Content-Type header
+	request.Header.Set("Content-Type", "application/json")
+	//create a http client
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err, nil)
+		return
+	}
+	defer response.Body.Close()
+
+	// create a varabiel we'll read response.Body into
+	var jsonFromService jsonResponse
+
+	// decode the json from the auth service
+	err = json.NewDecoder(response.Body).Decode(&jsonFromService)
+	if err != nil {
+		app.errorJSON(w, err, nil)
+		return
+	}
+
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New(jsonFromService.Message), nil, response.StatusCode)
+		return
+	}
+
+	var payload jsonResponse
+	payload.Error = jsonFromService.Error
+	payload.StatusCode = jsonFromService.StatusCode
+	payload.Message = jsonFromService.Message
+	payload.Data = jsonFromService.Data
+
+	app.writeJSON(w, http.StatusOK, payload)
+}
+
+type VerifyPaystackTransactionPayload struct {
+	Reference string `json:"reference"`
+}
+
+func (app *Config) VerifyPaystackTransaction(w http.ResponseWriter, r *http.Request) {
+
+	// verify the user token
+	user, err := app.getToken(r)
+	if err != nil {
+		app.errorJSON(w, err, user.Data, http.StatusUnauthorized)
+		return
+	}
+
+	if user.Error {
+		app.errorJSON(w, errors.New(user.Message), user.Data, user.StatusCode)
+		return
+	}
+
+	//extract the request body
+	var requestPayload VerifyPaystackTransactionPayload
+
+	//extract the request body
+	err = app.readJSON(w, r, &requestPayload)
+	if err != nil {
+		app.errorJSON(w, err, nil)
+		return
+	}
+
+	//create some json we will send to authservice
+	jsonData, _ := json.MarshalIndent(requestPayload, "", "\t")
+
+	invServiceUrl := fmt.Sprintf("%s%s", os.Getenv("PAYMENT_SERVICE_URL"), "verify-paystack-transaction")
+
+	// call the service by creating a request
+	request, err := http.NewRequest("POST", invServiceUrl, bytes.NewBuffer(jsonData))
+
+	if err != nil {
+		log.Println(err)
+		app.errorJSON(w, err, nil)
+		return
+	}
+
+	// Set the Content-Type header
+	request.Header.Set("Content-Type", "application/json")
+	//create a http client
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		log.Println(err)
+		app.errorJSON(w, err, nil)
+		return
+	}
+	defer response.Body.Close()
+
+	// create a varabiel we'll read response.Body into
+	var jsonFromService jsonResponse
+
+	// decode the json from the auth service
+	err = json.NewDecoder(response.Body).Decode(&jsonFromService)
+	if err != nil {
+		app.errorJSON(w, err, nil)
+		return
+	}
+
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New(jsonFromService.Message), nil, response.StatusCode)
+		return
+	}
+
+	var payload jsonResponse
+	payload.Error = jsonFromService.Error
+	payload.StatusCode = jsonFromService.StatusCode
+	payload.Message = jsonFromService.Message
+	payload.Data = jsonFromService.Data
+
+	app.writeJSON(w, http.StatusOK, payload)
+}
+
 func (app *Config) GetPlans(w http.ResponseWriter, r *http.Request) {
 
 	invServiceUrl := fmt.Sprintf("%s%s", os.Getenv("PAYMENT_SERVICE_URL"), "plans")
